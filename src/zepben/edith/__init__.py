6f09fc4a6fc4e390fd8c43bbbcf935ccaf59d150
Marcus Koh <marcus.koh@zepben.com>
2022-09-26 13:10:39 +1000

    Add logging
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/zepben/edith/__init__.py
+++ src/zepben/edith/__init__.py
@@ -4,20 +4,23 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import itertools
+import logging
 import random
 from asyncio import get_event_loop
 from zepben.evolve import *
 from zepben.protobuf.nc.nc_requests_pb2 import INCLUDE_ENERGIZED_LV_FEEDERS
 
+logger = logging.getLogger("zepben.edith")
+
 
 def do_nothing_allocator(_1, _2):
-    pass
+    return 0
 
 
 def distribution_transformer_proportional_allocator_creator(
         proportion: int,
         edith_customers: List[str]
-) -> Callable[[LvFeeder], None]:
+) -> Callable[[LvFeeder, NameType], int]:
     """
     Creates an allocator for the synthetic feeder creator that distributes a `proportion` of NMIs from `edith_customers`
     to an `LvFeeder`
@@ -31,17 +34,28 @@ def distribution_transformer_proportional_allocator_creator(
         raise ValueError("Proportion must be between 1 and 100")
 
     nmi_generator = itertools.cycle(edith_customers)
+    num_nmis_used = 0
 
-    def allocator(lv_feeder: LvFeeder, nmi_name_type: NameType):
+    def allocator(lv_feeder: LvFeeder, nmi_name_type: NameType) -> int:
+        nonlocal num_nmis_used
+
         usage_points = []
         for eq in lv_feeder.equipment:
             usage_points.extend(eq.usage_points)
-
         usage_points.sort(key=lambda up: up.mrid)
 
-        for up in random.sample(usage_points, int(len(usage_points) * proportion/100)):
+        usage_points_to_name = random.sample(usage_points, int(len(usage_points) * proportion / 100))
+        if num_nmis_used <= len(edith_customers) < num_nmis_used + len(usage_points_to_name):
+            logger.warning(
+                "Insufficient NMIs to allocate to the requested percentage of usage points without repetition."
+            )
+        num_nmis_used += len(usage_points_to_name)
+
+        for up in usage_points_to_name:
             # noinspection PyArgumentList
             up.add_name(nmi_name_type.get_or_add_name(next(nmi_generator), up))
+
+        return len(usage_points_to_name)
 
     return allocator
 
@@ -49,7 +63,7 @@ def distribution_transformer_proportional_allocator_creator(
 async def _create_synthetic_feeder(
         self: NetworkConsumerClient,
         feeder_mrid: str,
-        allocator: Callable[[LvFeeder, NameType], None] = do_nothing_allocator,
+        allocator: Callable[[LvFeeder, NameType], int] = do_nothing_allocator,
         seed: Optional[int] = None
 ) -> NetworkService:
     """
@@ -72,8 +86,11 @@ async def _create_synthetic_feeder(
     if seed:
         random.seed(seed)
 
+    total_allocated = 0
     for lv_feeder in sorted(feeder_network.objects(LvFeeder), key=lambda lvf: lvf.mrid):
-        allocator(lv_feeder, nmi_name_type)
+        total_allocated += allocator(lv_feeder, nmi_name_type)
+
+    logger.info(f"NMI names have been added to {total_allocated} usage points.")
 
     return feeder_network
 
@@ -83,7 +100,7 @@ NetworkConsumerClient.create_synthetic_feeder = _create_synthetic_feeder
 def _sync_create_synthetic_feeder(
         self: SyncNetworkConsumerClient,
         feeder_mrid: str,
-        allocator: Callable[[LvFeeder, NameType], None] = do_nothing_allocator,
+        allocator: Callable[[LvFeeder, NameType], int] = do_nothing_allocator,
         seed: Optional[int] = None
 ) -> NetworkService:
     return get_event_loop().run_until_complete(_create_synthetic_feeder(self, feeder_mrid, allocator, seed))
